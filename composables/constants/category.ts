fe87b1999c3f1064379cbd0a613069a514cc6346
almasblqis <almasblqis@gmail.com>
2023-10-27 07:45:17 +0700

    create project nuxt

---
+++ composables/constants/category.ts
@@ -0,0 +1,26 @@
+export const category: Category[] = [
+    {
+        id: 1,
+        name: "Face Wash",
+    },
+    {
+        id: 2,
+        name: "Moisturizer",
+    },
+    {
+        id: 3,
+        name: "Sunscreen",
+    },
+    {
+        id: 4,
+        name: "Face Mask",
+    },
+    {
+        id: 5,
+        name: "Body Serum",
+    }
+]
+export interface Category {
+    id?: number,
+    name?: string,
+}
